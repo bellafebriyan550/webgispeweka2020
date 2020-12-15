5e98f5d49948ce6cf21366a8440da9c14bcee00c
bellafebriyan550 <76066174+bellafebriyan550@users.noreply.github.com>
2020-12-16 01:27:42 +0700

    Add files via upload

---
+++ data/Tubantitikacuan_2.js
@@ -0,0 +1,8 @@
+var json_Tubantitikacuan_2 = {
+"type": "FeatureCollection",
+"name": "Tubantitikacuan_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 112.045665616817445, -6.899443500443392 ] } }
+]
+}
